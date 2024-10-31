be034ccaf1ffb8cee88aee7ef589468611d07976
jamiul-islam <jamiulislamjami@hotmail.com>
2024-10-31 21:59:15 +0600

    implemented favorite linting and formatting settings

--- storefront/settings.py
+++ storefront/settings.py
@@ -33,8 +33,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'django.contrib.sessions',
     'django.contrib.auth',
-    'django.contrib.contenttypes',    
+    'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground',
